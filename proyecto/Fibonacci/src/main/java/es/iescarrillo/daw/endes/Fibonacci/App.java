d0aa03e0fa29b4bf4dbfc472573eb2f59ebb8d67
rrammed082 <116265042+rrammed082@users.noreply.github.com>
2022-12-03 15:02:34 +0100

    subida

---
+++ proyecto/Fibonacci/src/main/java/es/iescarrillo/daw/endes/Fibonacci/App.java
@@ -0,0 +1,33 @@
+package es.iescarrillo.daw.endes.Fibonacci;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+        int numero,fibo1,fibo2,i;
+  
+        do{
+            System.out.print("Introduce numero mayor que 1: ");
+            numero = sc.nextInt();
+        }while(numero<=1);
+  
+        System.out.println("Los " + numero + " primeros terminos de la serie de Fibonacci son:");                 
+
+        fibo1=1;
+        fibo2=1;
+
+        System.out.print(fibo1 + " ");
+        for(i=2;i<=numero;i++){
+            System.out.print(fibo2 + " ");
+            fibo2 = fibo1 + fibo2;
+            fibo1 = fibo2 - fibo1;
+        }
+        System.out.println();
+    
+        sc.close();
+	}
+
+}
---
+++ proyecto/FibonacciMain/src/main/java/es/iescarrillo/daw/endes/FibonacciMain/ArchivoFuente.java
@@ -0,0 +1,10 @@
+package es.iescarrillo.daw.endes.FibonacciMain;
+
+import es.iescarrillo.daw.endes.Fibonacci.*;
+public class ArchivoFuente 
+{
+    public static void main( String[] args )
+    {
+        App.main(null);
+    }
+}
